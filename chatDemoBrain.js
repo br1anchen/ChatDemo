44ff510f406f18a17f78c2d3d1f04a71c8e146a5
Brian Chen <brianchen8990@gmail.com>
2012-11-01 16:14:57 +0100

    add basic chat websocket function

--- chatDemoBrain.js
+++ chatDemoBrain.js
@@ -1,5 +1,34 @@
-var socket = io.connect('http://localhost:8080');
-socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-});
+var socket = io.connect("http://localhost:8080");  
+  
+socket.on('quit', function (data) {  
+        status('Client ' + data.cid + ' quits!');  
+    });  
+  
+socket.on('join', function (data) {  
+        status('Client ' + data.cid + ' joins!');  
+    });  
+  
+socket.on('broadcast', function (data) {  
+        $('#thread').append($('<div>').html('client ' + data.cid + ' says:<br/>' + data.w));  
+    });  
+  
+function chat() {  
+    var words = $('#text').val();  
+    if($.trim(words)) {  
+        socket.emit('chat', {w: words});  
+        $('#text').val('');  
+    }  
+}  
+  
+function status(w) {  
+    $('#status').html(w);  
+}  
+  
+function initialize() {  
+    $(document).delegate('textarea', 'keydown', function (evt) {  
+            //console.info(evt.keyCode);  
+            if(evt.keyCode == 13 && evt.ctrlKey) {  
+                $('#send').focus().click();  
+            }  
+        });  
+}  
--- chat_server.js
+++ chat_server.js
@@ -1,25 +1,56 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
+var express = require('express');
+var app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server)
   , fs = require('fs')
 
-app.listen(8080);
+server.listen(8080);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/chatDemo.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading chatDemo.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.logger());
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret:"brianchencomoyodemo",
+    }));
 });
+
+var conns = {};
+
+io.sockets.on('connection', function (socket) {  
+        var cid = socket.id;  
+        for(var ccid in conns) {  
+            var soc = conns[ccid];  
+            soc.emit('join', {cid: socket.id});  
+        }  
+        conns[cid] = socket;  
+  
+        socket.on('disconnect', function () {  
+                delete conns[cid];  
+                for(var cid in conns) {  
+                    var soc = conns[cid];  
+                    soc.emit('quit', {cid: cid});  
+                }  
+            });  
+  
+        socket.on('chat', function (data) {  
+                data.cid = cid;  
+                for(var ccid in conns) {  
+                    var soc = conns[ccid];  
+                    soc.emit('broadcast', data);  
+                }  
+            });  
+});
+
+app.get('/', function (req, res) {  
+        res.sendfile(__dirname + '/chatDemo.html');  
+    });
+  
+app.use('/', express.static(__dirname + '/'));
+
+console.log('server start on http://localhost:8080'); 
+
+
